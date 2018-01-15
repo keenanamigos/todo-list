837bc6ef2ef3c396d85f13051a8750b5a6f5bdfc
Keenan Johnson <keenan.johnson1@outlook.com>
2018-01-15 13:40:44 -0600

    Setup Mongoose and Bluebird in app.js

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const path = require("path");
 const favicon = require("serve-favicon");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended": "false"}));
 app.use(express.static(path.join(__dirname, "dist")));
+
+mongoose.Promise = require("bluebird");
+mongoose.connect("mongodb://localhost/todo-list", { useMongoClient: true, promiseLibrary: require("bluebird")})
+    .then(() => console.log("Connection successful!"))
+    .catch((error) => console.error(error));
 
 
 module.exports = app;
